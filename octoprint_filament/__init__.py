cb3f92b61199f8036b2ae6468a11dbed5bd5c17b
MoonshineSG <margaret@dolphin.local>
2016-06-29 10:24:53 +0800

    version change

--- octoprint_filament/__init__.py
+++ octoprint_filament/__init__.py
@@ -98,7 +98,7 @@ class FilamentSensorPlugin(octoprint.plugin.StartupPlugin,
 		)
 
 __plugin_name__ = "Filament Sensor"
-__plugin_version__ = "1.1"
+__plugin_version__ = "1.0.1"
 __plugin_description__ = "Use a filament sensor to pause printing when fillament runs out."
 
 def __plugin_load__():
