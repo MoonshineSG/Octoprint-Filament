628242ac1244099da920623a779ab3a7e4300ffe
ǝuıɥsuooɯ <github@ovidiu.me>
2016-10-04 08:37:25 +0800

    added missing version check

--- octoprint_filament/__init__.py
+++ octoprint_filament/__init__.py
@@ -118,3 +118,9 @@ def __plugin_load__():
 
 	global __plugin_implementation__
 	__plugin_implementation__ = FilamentSensorPlugin()
+	
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+	
