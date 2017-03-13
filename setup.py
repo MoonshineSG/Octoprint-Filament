bbb38ce4a1f6967131ee78bf2821d0f6a89701f2
Allen McAfee <allen@robo3d.com>
2017-03-13 16:45:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "ovidiu"
 plugin_author_email = "github@ovidiu.me"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/MoonshineSG/OctoPrint-Filament"
+plugin_url = "https://github.com/Robo3D/OctoPrint-Filament"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
