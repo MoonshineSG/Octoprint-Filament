aa35acb45c618b5b25e5bda5120337a55e716e65
Ximidar <Ximidar@gmail.com>
2017-03-16 23:19:54 +0000

    Made the filament sensor work a little better

---
+++ octoprint_filament/PinMonitor.py
@@ -0,0 +1,94 @@
+import threading 
+import multiprocessing
+import sys
+
+import requests
+
+import RPi.GPIO as GPIO 
+import logging
+
+import requests
+import json
+
+class pinMonitor():
+    def __init__(self, printer_api, switch_pin):
+        self.switch_pin = switch_pin
+        self.threadID = "pin_monitor"
+        self.api_key = printer_api
+        self.paused = False
+        self.counter1 = 0
+        self.counter0 = 0
+        self.lock_1_timer = False
+        self.parent, self.child = multiprocessing.Pipe()
+        self.monitor = None
+        logging.basicConfig(filename='/home/pi/.octoprint/logs/octoprint.log', level=logging.DEBUG)
+        self.logger = logging
+        self.exit = False
+
+    def stop_monitor(self):
+        self.logger.info("Stopping monitor! ########################")
+        self.exit = True
+        if self.monitor != None:
+            for child in multiprocessing.active_children():
+                self.logger.info("Killed Child")
+                child.terminate()
+
+    def start_monitor(self):
+        self.logger.info("Starting monitor! ########################")
+        self.monitor = multiprocessing.Process(target=self.run_monitor, args=(self.child,))
+        self.monitor.start()
+
+    def monitor_pipe(self):
+        poll = self.parent.poll()
+        self.logger.info(str(poll) + "###########")
+        return poll
+        
+
+    def run_monitor(self, child_pipe):
+        self.initialize_pin()
+        self.monitor_pin(child_pipe)
+
+    def initialize_pin(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.switch_pin, GPIO.IN, GPIO.PUD_UP)
+
+    def monitor_pin(self, child):
+        
+        while not self.paused and not self.exit:
+            state = GPIO.input(21)
+            if state ==1:
+                self.counter1 +=1
+
+                if not self.lock_1_timer:
+                    self.lock_1_timer = True
+                    t = threading.Timer(10.0, self.check_count,args=(self.counter1,))
+                    t.start()
+
+        self.logger.info("While looped Stopped #################")
+        #Stop the print
+        if self.paused:
+            header = {'Content-Type': 'application/json', 'X-Api-Key': self.api_key}
+            payload = {'command': 'pause','action': 'pause'}
+            url = "http://127.0.0.1/api/job"
+            
+            r = requests.post(url, headers=header, data=json.dumps(payload))
+            self.logger.info(r.text)
+            child.send(True)
+        elif self.exit:
+            self.logger.info("Process " + str(os.getpid()) +" Exiting" )
+            sys.exit()
+            
+
+
+    def check_count(self, original_count):
+        self.logger.info("Checking Count " + str(self.counter1 - original_count) + " ###################################")
+        if self.counter1 - original_count > 100000:
+            self.logger.info("Pausing the Print!")
+            self.paused = True
+            
+        else:
+            self.lock_1_timer = False
+        
+        
+
--- octoprint_filament/__init__.py
+++ octoprint_filament/__init__.py
@@ -11,112 +11,118 @@ from flask import jsonify, request
 import logging
 import logging.handlers
 import RPi.GPIO as GPIO 
+from .PinMonitor import pinMonitor
 
 class FilamentSensorPlugin(octoprint.plugin.StartupPlugin,
-							octoprint.plugin.SettingsPlugin,
-							octoprint.plugin.EventHandlerPlugin,
-							octoprint.plugin.BlueprintPlugin):
+                           octoprint.plugin.ShutdownPlugin,
+                            octoprint.plugin.SettingsPlugin,
+                            octoprint.plugin.EventHandlerPlugin,
+                            octoprint.plugin.BlueprintPlugin):
 
-	def initialize(self):
-		self._logger.setLevel(logging.DEBUG)
-		
-		self._logger.info("Running RPi.GPIO version '{0}'...".format(GPIO.VERSION))
-		if GPIO.VERSION < "0.6":
-			raise Exception("RPi.GPIO must be greater than 0.6")
-			
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setwarnings(False)
-		self.we_paused = False
-		
-		self._logger.info("Filament Sensor Plugin [%s] initialized..."%self._identifier)
+    def __init__(self):
+        super(FilamentSensorPlugin, self).__init__()
+        self.pin_monitor = None
 
-	def on_after_startup(self):
-		self.PIN_FILAMENT = self._settings.get(["pin"])
-		self.BOUNCE = self._settings.get_int(["bounce"])
-		
-		if self.PIN_FILAMENT != -1:
-			self._logger.info("Filament Sensor Plugin setup on GPIO [%s]...###################"%self.PIN_FILAMENT)
-			GPIO.setup(self.PIN_FILAMENT, GPIO.IN)
-			self._logger.info("Starting CALLBACK##################################")
-			GPIO.add_event_detect(self.PIN_FILAMENT, GPIO.FALLING, callback=self.check_gpio, bouncetime=self.BOUNCE) 
-			GPIO.add_event_detect(self.PIN_FILAMENT, GPIO.RISING, callback=self.check_gpio, bouncetime=self.BOUNCE)
+    def initialize(self):
+        self._logger.setLevel(logging.DEBUG)
+        
+        self._logger.info("Running RPi.GPIO version '{0}'...".format(GPIO.VERSION))
+        if GPIO.VERSION < "0.6":
+            raise Exception("RPi.GPIO must be greater than 0.6")
+            
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        self.we_paused = False
+        
+        self._logger.info("Filament Sensor Plugin [%s] initialized..."%self._identifier)
 
-		
-	def get_settings_defaults(self):
-		return dict(
-			pin = -1,
-			bounce = 300
-		)
+    def on_after_startup(self):
+        self.PIN_FILAMENT = self._settings.get(["pin"])
+        self.BOUNCE = self._settings.get_int(["bounce"])
+        
+        if self.PIN_FILAMENT != -1:
+            self._logger.info("Filament Sensor Plugin setup on GPIO [%s]...###################"%self.PIN_FILAMENT)
+            self._logger.info("Starting CALLBACK##################################")
 
-	@octoprint.plugin.BlueprintPlugin.route("/status", methods=["GET"])
-	def check_status(self):
-		status = "-1"
-		if self.PIN_FILAMENT != -1:
-			status = "1" if GPIO.input(self.PIN_FILAMENT) else "0"
-		return jsonify( status = status )
-		
-	def on_event(self, event, payload):
-		# if event == Events.PRINT_STARTED:
-		# 	self._logger.info("Printing started. Filament sensor enabled.")
-		# 	self.setup_gpio()
-		# elif event in (Events.PRINT_DONE, Events.PRINT_FAILED, Events.PRINT_CANCELLED):
-		# 	self._logger.info("Printing stopped. Filament sensor disbaled.")
-		# 	try:
-		# 		GPIO.remove_event_detect(self.PIN_FILAMENT)
-		# 	except:
-		# 		pass
-		# elif event == Events.PRINT_RESUMED:
-		# 	self.we_paused = False
-		pass
+            api_key = self._settings.global_get(['api', 'key'])
+            self._logger.info(api_key)
+            self.pin_monitor = pinMonitor(api_key, self.PIN_FILAMENT)
+            
+    def on_shutdown(self):
+        if self.pin_monitor != None:
+            self.pin_monitor.stop_monitor() 
 
-	def setup_gpio(self):
-		try:
-			GPIO.remove_event_detect(self.PIN_FILAMENT)
-		except:
-			pass
-		if self.PIN_FILAMENT != -1:
-			GPIO.add_event_detect(self.PIN_FILAMENT, GPIO.FALLING, callback=self.check_gpio, bouncetime=self.BOUNCE) 
+        
+    def get_settings_defaults(self):
+        return dict(
+            pin = -1,
+            bounce = 300
+        )
 
+        
+    def on_event(self, event, payload):
+        if event == Events.PRINT_STARTED:
+            if self.pin_monitor != None:
+                self.pin_monitor.start_monitor()
+            else:
+                api_key = self._settings.global_get(['api', 'key'])
+                self.pin_monitor = pinMonitor(api_key, self.PIN_FILAMENT)
+                self.pin_monitor.start_monitor()
 
-	def check_gpio(self, channel):
-		state = GPIO.input(self.PIN_FILAMENT)
-		self._logger.debug("Detected sensor [%s] state [%s]? !"%(channel, state))
-		if not state: #safety pin ?
-			self._logger.debug("Sensor [%s]!"%state)
-			if self._printer.is_printing():
-				self._printer.toggle_pause_print()
-				self.we_paused  = True
+        elif event in (Events.PRINT_DONE, Events.PRINT_FAILED, Events.PRINT_CANCELLED, Events.PRINT_PAUSED):
+            if self.pin_monitor != None:
+                self.pin_monitor.stop_monitor()
 
-	def check_filament_pause(self):
-		return self.we_paused
+        elif event == Events.PRINT_RESUMED:
+            self.we_paused = False
+            if self.pin_monitor != None:
+                self.pin_monitor.stop_monitor()
+                self.pin_monitor = None
+                api_key = self._settings.global_get(['api', 'key'])
+                self.pin_monitor = pinMonitor(api_key, self.PIN_FILAMENT)
+                self.pin_monitor.start_monitor()
+            else:
+                api_key = self._settings.global_get(['api', 'key'])
+                self.pin_monitor = pinMonitor(api_key, self.PIN_FILAMENT)
+                self.pin_monitor.start_monitor()
+        
+    def check_filament_pause(self):
+        if self.pin_monitor != None:
+            for x in range(0,10):
+                poll = self.pin_monitor.monitor_pipe()
+                if poll:
+                    return poll
+                    break
+        else:
+            return False
 
-	def get_version(self):
-		return self._plugin_version
+    def get_version(self):
+        return self._plugin_version
 
-	def get_update_information(self):
-		return dict(
-			octoprint_filament=dict(
-				displayName="Filament Sensor",
-				displayVersion=self._plugin_version,
+    def get_update_information(self):
+        return dict(
+            octoprint_filament=dict(
+                displayName="Filament Sensor",
+                displayVersion=self._plugin_version,
 
-				# version check: github repository
-				type="github_release",
-				user="MoonshineSG",
-				repo="OctoPrint-Filament",
-				current=self._plugin_version,
+                # version check: github repository
+                type="github_release",
+                user="MoonshineSG",
+                repo="OctoPrint-Filament",
+                current=self._plugin_version,
 
-				# update method: pip
-				pip="https://github.com/MoonshineSG/OctoPrint-Filament/archive/{target_version}.zip"
-			)
-		)
+                # update method: pip
+                pip="https://github.com/MoonshineSG/OctoPrint-Filament/archive/{target_version}.zip"
+            )
+        )
 
 __plugin_name__ = "Filament Sensor"
 __plugin_version__ = "1.0.1"
 __plugin_description__ = "Use a filament sensor to pause printing when fillament runs out."
 
 def __plugin_load__():
-	global __plugin_implementation__
-	global __plugin_helpers__
-	__plugin_implementation__ = FilamentSensorPlugin()
-	__plugin_helpers__ = dict(check_auto_pause=__plugin_implementation__.check_filament_pause)
+    global __plugin_implementation__
+    global __plugin_helpers__
+    __plugin_implementation__ = FilamentSensorPlugin()
+    __plugin_helpers__ = dict(check_auto_pause=__plugin_implementation__.check_filament_pause)
 
